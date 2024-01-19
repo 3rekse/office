7707aceab347a4510145ff4678a5c960b5c33fdd
3rekse <fabrizio.bonfiglio@scuola.istruzione.it>
2024-01-19 10:23:26 +0100

    Updates

--- assets/PresenterPrint-07d1f704.js
+++ assets/PresenterPrint-07d1f704.js
@@ -1,4 +1,4 @@
-import{d,u as _,a as p,c as m,b as h,r as u,e as s,f as t,t as a,g as r,F as f,h as g,n as v,i as x,o as n,j as y,k as b,l as k,m as N,_ as P}from"./index-1b74615a.js";import{N as w}from"./NoteDisplay-38531900.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),M={key:0,class:"border-gray-400/50 mb-8"},j=d({__name:"PresenterPrint",setup(z){_(`
+import{d,u as _,a as p,c as m,b as h,r as u,e as s,f as t,t as a,g as r,F as f,h as g,n as v,i as x,o as n,j as y,k as b,l as k,m as N,_ as P}from"./index-cc4e2c5d.js";import{N as w}from"./NoteDisplay-52c778f1.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),M={key:0,class:"border-gray-400/50 mb-8"},j=d({__name:"PresenterPrint",setup(z){_(`
 @page {
   size: A4;
   margin-top: 1.5cm;
